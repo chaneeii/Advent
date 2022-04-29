8ad3f4ed5fd9f35239cfbd37bebdf066bf974194
Chanhee Jeong <chan420420@gmail.com>
2022-04-29 14:44:02 +0900

    Initial Commit

---
+++ AdventCalendar/AdventCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventCalendarApp.swift
+//  AdventCalendar
+//
+//  Created by Chanhee Jeong on 2022/04/29.
+//
+
+import SwiftUI
+
+@main
+struct AdventCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdventCalendar
+//
+//  Created by Chanhee Jeong on 2022/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
